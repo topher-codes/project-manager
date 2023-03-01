8833154385cf01d80dd715b0c4fc0ab39e9567cc
Chris Andrews <andrewsc33@icloud.com>
2023-03-01 09:50:32 -0800

    updated sidebar

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,7 +1,10 @@
+import Sidebar from "@/components/Sidebar";
+
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <Sidebar />
     </div>
   );
 };
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,6 +1,5 @@
 import Card from "./Card";
 import Image from "next/image";
-import logo from "@/assets/images/logo.png";
 import SidebarLink from "./SidebarLink";
 
 const links = [
@@ -21,9 +20,11 @@ const links = [
 const Sidebar = () => {
   return (
     <Card className="h-full w-40 flex items-center justify-between flex-wrap">
-      <div className="w-full flex justify-center items-center">
-        <Image src={logo} alt="Able logo" priority className="w-14" />
-      </div>
+      {
+        // <div className="w-full flex justify-center items-center">
+        //<Image src={logo} alt="Able logo" priority className="w-14" />
+        //</div>
+      }
       {links.map((link) => (
         <SidebarLink link={link} />
       ))}
