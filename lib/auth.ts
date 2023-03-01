5be2f0d36a7d227b360e6527634838bffb80b8a3
Chris Andrews <andrewsc33@icloud.com>
2023-03-01 11:37:46 -0800

    auth updated

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,28 @@
 import bcrypt from "bcrypt";
+import { SignJWT, jwtVerify } from "jose";
+import { db } from "./db";
 
-export function hashPassword(password) {
-  return bcrypt.hashSync(password, 10);
-}
+export const hashPassword = (password) => bcrypt.hash(password, 10);
+
+export const comparePasswords = (plainTextPassword, hashedPassword) =>
+  bcrypt.compare(plainTextPassword, hashedPassword);
+
+export const createJWT = (user) => {
+  const iat = Math.floor(Date.now() / 1000);
+  const exp = iat + 60 * 60 * 24 * 7; // 7 days
+
+  return new SignJWT({ payload: { id: user.id, email: user.email } })
+    .setProtectedHeader({ alg: "HS256", typ: "JWT" })
+    .setExpirationTime(exp)
+    .setIssuedAt(iat)
+    .setNotBefore(iat)
+    .sign(new TextEncoder().encode(process.env.JWT_SECRET));
+};
+
+export const verifyJWT = async (jwt) => {
+  const { payload } = await jwtVerify(
+    jwt,
+    new TextEncoder().encode(process.env.JWT_SECRET)
+  );
+  return payload.payload as any;
+};
--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   experimental: {
     appDir: true,
+    serverComponentsExternalPackages: ["bcrypt"],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
