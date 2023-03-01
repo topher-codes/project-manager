8c3ba1cd51cf0dae811219935284853f28ecca7e
Chris Andrews <andrewsc33@icloud.com>
2023-03-01 11:11:48 -0800

    added authform to register page

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import Authform from "@/components/Authform";
+
+const Register = () => {
+  return (
+    <div>
+      <Authform mode="register" />
+    </div>
+  );
+};
+
+export default Register;
