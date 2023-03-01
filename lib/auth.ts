e72bbd5bed55b9c4f6f50bc50feb707736d97051
Chris Andrews <andrewsc33@icloud.com>
2023-03-01 11:43:49 -0800

    finished auth handler

--- lib/auth.ts
+++ lib/auth.ts
@@ -19,10 +19,22 @@ export const createJWT = (user) => {
     .sign(new TextEncoder().encode(process.env.JWT_SECRET));
 };
 
-export const verifyJWT = async (jwt) => {
+export const validateJWT = async (jwt) => {
   const { payload } = await jwtVerify(
     jwt,
     new TextEncoder().encode(process.env.JWT_SECRET)
   );
   return payload.payload as any;
+};
+
+export const getUserFromCookie = async (cookies) => {
+  const jwt = cookies.get(process.env.COOKIE_NAME);
+
+  const { id } = await validateJWT(jwt);
+
+  const user = await db.user.findUnique({
+    where: { id },
+  });
+
+  return user;
 };
