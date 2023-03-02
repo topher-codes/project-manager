3bb3e95db8b6ce501a457a87678834a499b5adb0
Chris Andrews <andrewsc33@icloud.com>
2023-03-02 12:18:59 -0800

    api types added

--- pages/api/signin.ts
+++ pages/api/signin.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 import { db } from "@/lib/db";
 import { comparePasswords, createJWT } from "@/lib/auth";
 import { serialize } from "cookie";
