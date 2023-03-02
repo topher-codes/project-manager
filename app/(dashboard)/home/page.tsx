ce90f90c2a7716ca899e4ba050068774cf959172
Chris Andrews <andrewsc33@icloud.com>
2023-03-01 18:51:16 -0800

    created project card component

--- app/(dashboard)/home/page.tsx
+++ app/(dashboard)/home/page.tsx
@@ -1,5 +1,6 @@
 /*import { delay } from "@/lib/async";*/
 import Greeting from "@/components/Greeting";
+import GreetingSkeleton from "@/components/GreetingSkeleton";
 import { getUserFromCookie } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { cookies } from "next/headers";
@@ -8,16 +9,18 @@ import { Suspense } from "react";
 
 export default async function Page() {
   return (
-    <div className="h-full overflow-y-auto pr-6 w-1/1">
+    <div className="w-full h-full pr-6 overflow-y-auto">
       <div className=" h-full  items-stretch justify-center min-h-[content]">
-        <div className="flex-1 grow flex">
-          <Greeting />
+        <div className="flex flex-1 grow">
+          <Suspense fallback={<GreetingSkeleton />}>
+            <Greeting />
+          </Suspense>
         </div>
-        <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
+        <div className="flex flex-wrap items-center mt-3 -m-3 flex-2 grow ">
           {/** projects map here */}
           <div className="w-1/3 p-3">{/* new project here */}</div>
         </div>
-        <div className="mt-6 flex-2 grow w-full flex">
+        <div className="flex w-full mt-6 flex-2 grow">
           <div className="w-full">{/* tasks here */}</div>
         </div>
       </div>
--- components/Greeting.tsx
+++ components/Greeting.tsx
@@ -13,10 +13,10 @@ const getData = async () => {
 const Greeting = async () => {
   const user = await getData();
   return (
-    <Card className="w-full py-4 relative">
+    <Card className="relative w-full py-4">
       <div className="mb-4">
-        <h1 className="text-3xl text-gray-700 font-bold mb-4">
-          Hello, {user.firstName}!
+        <h1 className="mb-4 text-3xl font-bold text-gray-700">
+          Hello, {user?.firstName}!
         </h1>
         <h4 className="text-xl text-gray-400">
           Check your daily tasks and schedule
